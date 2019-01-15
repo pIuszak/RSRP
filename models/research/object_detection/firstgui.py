00749b1a414b191ba9818c4b1f576b6cb8bbb024
HYPERBOOKADAM\adamm <adammak2342@o2.pl>
2019-01-15 22:11:05 +0100

    Temp gui images

--- models/research/object_detection/firstgui.py
+++ models/research/object_detection/firstgui.py
@@ -1,6 +1,39 @@
 import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+
+sys.path.append("..")
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from tkinter import *
+MODEL_NAME = 'inference_graph'
+CWD_PATH = os.getcwd()
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
+NUM_CLASSES = 57
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 class App:
     def __init__(self, window, window_title, video_source=0):
@@ -9,12 +42,34 @@ class App:
         self.video_source = video_source
         # open video source
         self.vid = MyVideoCapture(video_source)
- 
+
+
+        
         # Create a canvas that can fit the above video source size
-        self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas = Canvas(window, width = self.vid.width, height = self.vid.height+200)
         self.canvas.pack()
+
+        botFrame = Frame(window)
+        botFrame.pack(side=BOTTOM)
+
+        img = PIL.Image.open("GUI\stop.png")
+        img = img.resize((100,100), PIL.Image.ANTIALIAS)
+        stop = PIL.ImageTk.PhotoImage(img)
+        #stop = PhotoImage(file="GUI\stop.png")
+
+        label1 = Label(botFrame, text = "Label stop", image=stop)
+
+        
+        label2 = Label(botFrame, text = "Label 2")
+        label1.grid(row = 0, column = 0, sticky='nsew')
+        label2.grid(row = 1, column = 1, sticky='nsew')
+
+        
+        #self.canvas.create_window(100,100, window=label1)
+        
+        
         # After it is called once, the update method will be automatically called every delay milliseconds
-        self.delay = 15
+        self.delay = 30
         self.update()
         self.window.mainloop()
 
@@ -22,6 +77,23 @@ class App:
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
         if ret:
+            frame_expanded = np.expand_dims(frame, axis=0)
+            # Perform the actual detection by running the model with the image as input
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: frame_expanded})
+            
+            # Draw the results of the detection (aka 'visulaize the results')
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                frame,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8,
+                min_score_thresh=0.60)
+            
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0,0,image=self.photo, anchor=tkinter.NW)
         self.window.after(self.delay, self.update)
